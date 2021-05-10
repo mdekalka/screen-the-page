ed65714e9cb5aa55aababa7362fdbbfff227410e
Aleh Isakau <isakaualeh@gmail.com>
2021-05-11 00:08:03 +0300

    implementing puppeteer to scrap page and take screen

--- client/script.js
+++ client/script.js
@@ -1,4 +1,66 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+;(function() {
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    const button = document.querySelector('.button');
+    const input = document.querySelector('.input');
+    const error = document.querySelector('.error');
+    const spinner = document.querySelector('.loading');
   
-});
+    button.addEventListener('click', () => {
+      const isValidUrl = validateHttpUrl(input.value);
+      
+      if (!isValidUrl) {
+        error.classList.add('show');
+        return;
+      }
 
+      const image = await httpRequest(input.value);
+
+    });
+
+    input.addEventListener('input', () => {
+      error.classList.remove('show');
+    })
+  });
+
+  function startLoading() {
+    spinner.classList.add('show');
+    button.disabled = true;
+    input.disabled = true;
+  }
+
+  function endLoading() {
+    spinner.classList.remove('show');
+    button.disabled = false;
+    input.disabled = false;
+  }
+
+  function validateHttpUrl(string) {
+    let url;
+    
+    try {
+      url = new URL(string);
+    } catch (e) {
+      console.log('validate url error message:', e.message);
+      return false;  
+    }
+  
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
+  async function loadImage(urlParam) {
+    const params = new URLSearchParams({ url: urlParam });
+
+    try {
+      const response = await fetch(`http://localhost:3002/screenshot?${params}`);
+      const data = await response.blob();
+
+      image = URL.createObjectURL(data);
+
+      return image;
+    } catch (e) {
+      console.log('load image failed:', e.message)
+      return null;
+    }
+  }
+
+})();
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const puppeteer = require('puppeteer');
 
 
 const PORT = 3002;
@@ -20,5 +21,29 @@ app.get('/', function(request, response) {
   response.sendFile('./client/index.html', { root: __dirname });
 });
 
-// SSE events handler
-// app.get('/events', eventsHandler);
+app.get('/screenshot', async function(request, response) {
+  const url = request.query.url;
+
+  const image = await htmlToImage(url);
+
+  // response.json({ image });
+  response.sendFile(image)
+})
+
+async function htmlToImage(url) {
+  if (!url) return;
+
+  const browser = await puppeteer.launch({ headless: true, slowMo: 100 });
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await page.screenshot({ fullPage: true, path: 'screen.png' });
+
+  await page.close();
+  await browser.close();
+
+  screenshotPath = path.normalize(`${process.cwd()}/screen.png`);
+
+  return screenshotPath;
+};
